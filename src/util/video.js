68366a3d12ae71673a24e7e25fcb4b114b27aabd
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-20 05:37:58 +0100

    Use synced methods

--- src/util/video.js
+++ src/util/video.js
@@ -1,16 +1,16 @@
-import { spawn } from 'child_process';
+import { spawnSync } from 'child_process';
 import fs from 'fs';
 
-export const init = () => {
-  console.log('Starting avconv...') // eslint-disable-line
-  while (true) {
-    const args = '-f video4linux2 -s 640x480 -i /dev/video0 -vframes 1 ~/out.jpg';
-    spawn('avconv', args.split(' '));
-  }
-};
-
 export const startStreaming = (socket, robotID) => {
-  fs.watch('~/out.jpg', () => {
-    console.log('change!');
-  });
+  while (true) {
+    console.log('taking photo');
+    const args = '-f video4linux2 -s 640x480 -i /dev/video0 -vframes 1 ~/out.jpg';
+    spawnSync('avconv', args.split(' '));
+
+    fs.watch('~/out.jpg', () => {
+      console.log('change!');
+    });
+  }
 }
+
+export default startStreaming;
