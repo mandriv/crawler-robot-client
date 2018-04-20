87fd511a41949610afa77e83278d7adb835572b8
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-20 04:20:44 +0100

    Fix new mp4 stream

--- src/util/video.js
+++ src/util/video.js
@@ -7,7 +7,6 @@ export const CAMERA_DIR = '/dev/video0';
 export const FRAMERATE = 25;
 export const VIDEO_SIZE = '640x480';
 export const FORMAT_OUT = 'h264';
-export const BITRATE = '1000k';
 export const URL = `http://localhost:${port}/stream`;
 
 export const DEFAULT_CONFIG = {
@@ -16,7 +15,6 @@ export const DEFAULT_CONFIG = {
   framerate: FRAMERATE,
   videoSize: VIDEO_SIZE,
   formatOut: FORMAT_OUT,
-  bitrate: BITRATE,
   url: URL,
 };
 
@@ -28,11 +26,10 @@ export const init = (cfg = DEFAULT_CONFIG) => {
   const sizeIn = `-video_size ${cfg.videoSize}`;
   const input = `-i ${cfg.dir}`;
   const formatOut = `-f ${cfg.formatOut}`;
-  const codec = '-codec:v mpeg1video';
+  const codec = `-codec:v ${cfg.formatOut}`;
   const sizeOut = `-s ${cfg.videoSize}`;
-  const bitrate = `-b:v ${cfg.bitrate} -bf 0`;
   const { url } = cfg;
-  const args = `${formatIn} ${framerate} ${sizeIn} ${input} ${formatOut} ${codec} ${sizeOut} ${bitrate} ${url}`;
+  const args = `${formatIn} ${framerate} ${sizeIn} ${input} ${formatOut} ${codec} ${sizeOut} ${url}`;
 
   spawn('avconv', args.split(' '));
 };
